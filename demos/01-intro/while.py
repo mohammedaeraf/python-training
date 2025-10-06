5ae400f5efb7086e9190def1e98cab5004533cda
Mohammed Aeraf <aeraf@outlook.com>
2025-10-06 15:12:23 +0530

    while loop demo

--- demos/01-intro/while.py
+++ demos/01-intro/while.py
@@ -1,4 +1,9 @@
+# while loop
 i = 0
-while i < 5:
-    print(i)
-    i += 1
+while i <= 10:
+    print(i)   # infinite loop
+    i = i + 1
+
+print("after while loop")
+
+# todo: Print numbers from 10 to 0
