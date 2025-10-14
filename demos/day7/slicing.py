0cdbf4ec27bdf515d4727f3b4239e253f0bece02
Mohammed Aeraf <aeraf@outlook.com>
2025-10-14 20:30:54 +0530

    slicing

---
+++ demos/day7/slicing.py
@@ -0,0 +1,22 @@
+list_main = [10, 20, 30, 40, 50, 60, 70]
+
+list1 = list_main[0:3]
+print(list1)
+
+list2 = list_main[3:6] 
+print(list2)
+
+list3 = list_main[:4]
+print(list3)
+
+list4 = list_main[4:]
+print(list4)
+
+list5 = list_main[::2]
+print(list5)
+
+list6 = list_main[1::2]
+print(list6)
+
+list7= list_main[::-1]
+print(list7)
