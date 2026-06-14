3396cfabc2f2a0f03c2fe0204e8a0819bc0eadd7
Mohammed Aeraf <aeraf@outlook.com>
2026-06-14 20:01:33 +0530

    Set Problems

---
+++ demos/set1.py
@@ -0,0 +1,8 @@
+numbers = {10, 20, 10, 30, 40, 30, 50}
+print(numbers)
+
+colors = {"red", "green", "blue"}
+
+# for-in loop
+for color in colors:
+    print(color, end=" ")
---
+++ demos/set2.py
@@ -0,0 +1,10 @@
+courses = {"Java","Python"}
+courses.add("React JS")
+print(courses)
+
+courses.update(["AI Tools", "Web Design"])
+print(courses)
+
+# courses.remove("Go Lang")
+courses.discard("AI Tools")
+print(courses)
